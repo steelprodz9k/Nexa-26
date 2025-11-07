ccf84bc4655b6c2cf108d68f2ca02063bb5c773e
steelprodz9k <petersonws123@outlook.com>
2025-11-07 13:44:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,29 @@ function openApp(app) {
 function closeApp(app) {
   document.getElementById(`window-${app}`).classList.add('hidden');
 }
+
+// Make windows draggable
+document.querySelectorAll('.draggable').forEach(el => {
+  el.onmousedown = function (e) {
+    let shiftX = e.clientX - el.getBoundingClientRect().left;
+    let shiftY = e.clientY - el.getBoundingClientRect().top;
+
+    function moveAt(pageX, pageY) {
+      el.style.left = pageX - shiftX + 'px';
+      el.style.top = pageY - shiftY + 'px';
+    }
+
+    function onMouseMove(e) {
+      moveAt(e.pageX, e.pageY);
+    }
+
+    document.addEventListener('mousemove', onMouseMove);
+
+    el.onmouseup = function () {
+      document.removeEventListener('mousemove', onMouseMove);
+      el.onmouseup = null;
+    };
+  };
+
+  el.ondragstart = () => false;
+});
